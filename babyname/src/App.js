8b9ca343d92045450914977d3cb994d218133486
fatma <bedizma@hotmail.com>
2022-01-15 12:45:35 +0000

    created my-exapmles file

---
+++ babyname/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Data from "./data.json";
+import './App.css';
+
+
+function App() {
+  const name = Data;
+console.log(name)
+  return (
+    <div className="App">Hello world</div>
+  );
+}
+
+export default App;
